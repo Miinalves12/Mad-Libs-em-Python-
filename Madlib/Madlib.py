6cf2cfed0f99df43be869ae9a06aed1d765cde89
Yasmin N Valentim <108993216+Miinalves12@users.noreply.github.com>
2023-08-03 16:43:44 -0700

    Add files via upload

---
+++ Madlib/Madlib.py
@@ -0,0 +1,10 @@
+#youtuber = "Malena"
+#print("subscribe to " + youtuber)
+#print("subscribe to {}".format(youtuber))
+#print(f"subscribe to {youtuber}")
+adj = input('Adjetivo: ')
+verb1 = input('Verbo:')
+verb2 = input ('Verbo: ')
+famous_person = input("Pessoa famosa: ")
+Madlib = f" Programação é tao {adj}! Me deixa tão animada o tempo todo porquê eu amo{verb1}. Fique hidratado e {verb2} como voce é a/o {famous_person}! "
+print(Madlib)
